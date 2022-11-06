e4057aa6b6d5916fc4d9d872f5bc4c7476477348
e-NAZ <victoriorox@gmail.com>
2022-11-06 04:06:37 +0100

    todo prj commits

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'mxhf8rh1vf'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-vico.us.auth0.com',            // Auth0 domain
+  clientId: 'Kj9yBURElqVlttRZnTyZtCPYvTLVbdLA',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
